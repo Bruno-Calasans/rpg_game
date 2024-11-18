1c9d2191b043f1416fe7a2a0db8376f00d52938b
bruno-calasans <brunogstsho@gmail.com>
2024-11-17 22:06:04 -0300

    create parallax background

---
+++ scripts/env/background.gd
@@ -0,0 +1,22 @@
+extends ParallaxBackground
+
+# it says if the parallax backgroound can move automatically
+@export_category('Background vars')
+@export var can_process = true
+
+# it says the speed of each layer of the parallax background from layer1 to layer4
+# from layer1 to layer4, the first layer is faster than the last one
+@export var layer_speed: Array[float] = [20, 15, 10, 5]
+
+func _ready() -> void:
+	if can_process == false: 
+		set_physics_process(false)
+		
+func _physics_process(delta: float) -> void:
+	for index in get_child_count() - 1:
+		var layer = get_child(index)
+		if layer is ParallaxLayer:
+			#+= positive if the player is moving to left (background moves to right)
+			#-= negative if the player is moving to right (background moves to left)
+			layer.motion_offset.x += delta * layer_speed[index]
+		
