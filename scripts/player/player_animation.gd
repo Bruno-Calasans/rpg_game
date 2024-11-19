b0d2554106d11445266c9fae1e69aaaa4c831e7b
bruno-calasans <brunogstsho@gmail.com>
2024-11-19 17:52:19 -0300

    fix jumping and landing animations

--- scripts/player/player_animation.gd
+++ scripts/player/player_animation.gd
@@ -2,10 +2,21 @@ extends AnimationPlayer
 class_name PlayerAnimation
 
 @onready var texture: Sprite2D = get_node('../Texture')
+@onready var player: Player = get_node('..')
 
 func animate(direction: Vector2):
 	verify_direction(direction)
-	horizontal_behavior(direction)
+	# player is falling or jumping
+	if direction.y != 0:
+		vertical_behavior(direction)
+		
+	elif player.landing == true:
+		play('landing')
+		# player is frozed while landing animation is playing
+		player.set_physics_process(false)
+	# player is idle or running
+	else:
+		horizontal_behavior(direction)
 	
 	#print('Animate: {direction}'.format({'direction': direction}))
 
@@ -27,3 +38,21 @@ func horizontal_behavior(velocity: Vector2):
 	else:
 		#play idle animation
 		play('idle')
+		
+func vertical_behavior(direction: Vector2):
+	# player is jumping
+	if  direction.y < 0:
+		player.landing = false
+		play("jump")
+	# player is falling
+	if direction.y > 0:
+		player.landing = true
+		play('fall')
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"landing":
+			player.landing = false
+			# player is unfrozed when landing animation stops
+			player.set_physics_process(true)
