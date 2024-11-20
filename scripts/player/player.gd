baec6f73b93a929582b7606a0781b9c25ae22f72
bruno-calasans <brunogstsho@gmail.com>
2024-11-19 21:21:51 -0300

    attach attack, parry and crouch animations to player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,30 +2,37 @@ extends CharacterBody2D
 class_name Player
 
 @onready var animation: AnimationPlayer = get_node("Animation")
-@export var speed: int = 150
-@export var jump_speed: int = 75
-@export var player_gravity: int = 1
-@export var jump_count: int = 0
+@export var speed = 150
+@export var jump_speed = 75
+@export var player_gravity = 1
+@export var jump_count = 0
 @export var landing = false
+@export var attacking = false
+@export var crouching = false
+@export var can_crouch_now = false
+@export var parrying = false
+@export var can_parry_now = false
+@export var can_move = true
 
 func horizontal_movement():
 	# if you press both right and left, the character doesn't move (-1 + 1)
 	# if you press only right, the character moves to right (0 + 1)
 	# if you press only left, the character moves to left (-1 + 0)
+	if not can_move:
+		velocity.x = 0
+		return
 	var horizontal_direction = - Input.get_action_strength('left') + Input.get_action_strength('right') 
 	velocity.x = horizontal_direction * speed
-	
-	#
-	animation.animate(velocity)
-	print('Velocity: {velocity}'.format({'velocity': velocity}))
+	#print('Velocity: {velocity}'.format({'velocity': velocity}))
 
 func vertical_moviment():
-	# it 
+	# it resets jump count
 	if is_on_floor():
 		jump_count = 0
 	
-	# it lmits the number max of jumps by 2
-	if Input.is_action_just_pressed('jump') and jump_count < 2:
+	# it lmits the number max of jumps by 2 and while not doind any other actions
+	var can_jump = jump_count < 2 and can_move
+	if Input.is_action_just_pressed('jump') and can_jump:
 		jump_count += 1
 		velocity.y = jump_speed
 		
@@ -35,10 +42,52 @@ func gravity(delta: float):
 	# it limits the falling speed or jumping speed
 	if velocity.y > player_gravity: velocity.y = player_gravity
 
+func attack():
+	var can_attack = not attacking and not crouching and not parrying
+	if Input.is_action_just_pressed('attack') and can_attack and is_on_floor():
+		attacking = true
+		can_move = false
+		
+func crouch():
+	var can_crouch = is_on_floor() and not parrying and not crouching
+	var can_stand = is_on_floor() and crouching and not parrying
+	
+	# crouch
+	if Input.is_action_pressed('crouch') and can_crouch :
+		crouching = true
+		can_move = false
+		can_crouch_now = true
+	
+	# standing
+	if Input.is_action_just_released('crouch') and can_stand:
+		crouching = false
+		can_move = true
+		can_crouch_now = false
+		
+func parry():
+	var can_parry = is_on_floor() and not parrying and not crouching
+	var can_stop_parry = is_on_floor() and parrying and not crouching
+	
+	if Input.is_action_pressed('parry') and can_parry:
+		parrying = true
+		can_move = false
+		can_parry_now = true
+		
+	if Input.is_action_just_released('parry') and can_stop_parry:
+		parrying = false
+		can_move = true
+		can_parry_now = false
+		
+func actions():
+	attack()
+	crouch()
+	parry()
+
 func _physics_process(delta: float) -> void:
 	horizontal_movement()
 	vertical_moviment()
 	gravity(delta)
+	actions()
 	
 	# A vector used to determine what's wall or floor
 	# by default, it's Vector2.UP
@@ -46,4 +95,7 @@ func _physics_process(delta: float) -> void:
 	
 	# move the player and return true if it encounters a collision
 	move_and_slide()
-	print(is_on_floor(), is_on_ceiling())
+	
+	#
+	animation.animate(velocity)
+	
--- scripts/player/player_animation.gd
+++ scripts/player/player_animation.gd
@@ -3,17 +3,25 @@ class_name PlayerAnimation
 
 @onready var texture: Sprite2D = get_node('../Texture')
 @onready var player: Player = get_node('..')
+var attack_suffix = '_left'
 
 func animate(direction: Vector2):
 	verify_direction(direction)
+	
+	# this actions have priority over the actions below it
+	if player.attacking or player.crouching or player.parrying:
+		action_behavior()
+		
 	# player is falling or jumping
-	if direction.y != 0:
+	elif direction.y != 0:
 		vertical_behavior(direction)
 		
+	# player is frozed while landing animation is playing
 	elif player.landing == true:
 		play('landing')
-		# player is frozed while landing animation is playing
+		# stop player while landing
 		player.set_physics_process(false)
+		
 	# player is idle or running
 	else:
 		horizontal_behavior(direction)
@@ -22,11 +30,15 @@ func animate(direction: Vector2):
 
 # it determines which position the player is looking at
 func verify_direction(direction: Vector2):
+	# going to right
 	if direction.x > 0:
 		texture.flip_h = false
-		
+		attack_suffix = '_right'
+			
+	# going to left
 	if direction.x < 0:
 		texture.flip_h = true
+		attack_suffix = '_left'
 		
 	#else:
 	#	flip_h = true
@@ -49,10 +61,28 @@ func vertical_behavior(direction: Vector2):
 		player.landing = true
 		play('fall')
 
-
+func action_behavior():
+	if player.attacking:
+		play('attack' + attack_suffix)
+			
+	elif player.crouching and player.can_crouch_now: 
+		play('crouch')
+		player.can_crouch_now = false
+		
+	elif player.parrying and player.can_parry_now: 
+		play('parry')
+		player.can_parry_now = false
+	
 func _on_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
 		"landing":
 			player.landing = false
 			# player is unfrozed when landing animation stops
 			player.set_physics_process(true)
+		"attack_right":
+			player.attacking = false
+			player.can_move = true
+		"attack_left":
+			player.attacking = false
+			player.can_move = true
+		
