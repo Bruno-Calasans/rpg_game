85418d29561f76e69da078532cc25e7569f46e06
bruno-calasans <brunogstsho@gmail.com>
2024-11-24 17:42:16 -0300

    add explosion to fireball spell

--- scripts/spell/fireball_spell.gd
+++ scripts/spell/fireball_spell.gd
@@ -6,7 +6,7 @@ var spell_damage = 0
 func _ready() -> void:
 	# starts the particles animation
 	for child in get_children():
-		if child is CPUParticles2D:
+		if child is CPUParticles2D and child.name != 'Explosion3':
 			child.emitting = true
 
 # remove particles after animation ends
